9977adf39ecc1bdeda7abd5c092f3c4f969bc27c
00jackson <95626883+00jackson@users.noreply.github.com>
2024-09-21 20:26:47 +0530

    fix loading

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Image from 'next/image';
 const Loading = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <img
+      <Image
         src="/imgs/logo.jpeg"
         alt="Loading..."
         className="w-48 h-48 object-contain"
