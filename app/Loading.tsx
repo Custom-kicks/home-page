26366278b518d528c697e3cebad58e8ecc3d2836
00jackson <95626883+00jackson@users.noreply.github.com>
2024-09-21 21:43:55 +0530

    fixes

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from 'next/legacy/image';
+import Image from 'next/image';
 const Loading = () => {
   return (
     <div className="flex items-center justify-center h-screen">
