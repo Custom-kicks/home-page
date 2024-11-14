336a2773af10443541d31259f78772caee06cc87
00jackson <95626883+00jackson@users.noreply.github.com>
2024-11-14 18:37:54 +0530

    .

--- components/LeadGenerationForm.tsx
+++ components/LeadGenerationForm.tsx
@@ -134,7 +134,7 @@ const LeadGenerationForm: React.FC = () => {
                       <svg className="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
                       </svg>
-                      <span>Congrats You're in</span>
+                      <span>Congrats You&#39;re in</span>
                     </span>
                   ) : (
                     "Get Discount"
