5d693ec04712b99481f7fc72058fd7c9763e4518
00jackson <95626883+00jackson@users.noreply.github.com>
2024-11-26 21:55:48 +0530

    .

--- app/FAQ/Faq.tsx
+++ app/FAQ/Faq.tsx
@@ -80,7 +80,7 @@ const FAQWithLeadForm: React.FC = () => {
         <div className="p-8 md:p-12 lg:px-16 lg:py-24">
           <div className="mx-auto max-w-lg text-center">
             <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">Stay Updated with Our Sneaker News!</h2>
-            <p className="text-gray-500 text-sm lg:text-xl mt-2 lg:mt-4 sm:block">
+            <p className="text-gray-500 text-xs md:text-sm lg:text-lg mt-2 lg:mt-4 sm:block">
               Subscribe to get exclusive discounts, early releases, and insights into the latest sneaker trends.
             </p>
           </div>
